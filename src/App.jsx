9cc60ff564c764774d88e152722253ebf011b5ac
montenegro <caio.bezerra@sharetecnologia.com.br>
2025-11-20 21:11:53 -0300

    tela de submissao pdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,21 @@
-import { useState } from "react";
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import LoginPage from "./pages/LoginPage";
 import RecoveryPage from "./pages/RecoveryPage";
 import RegisterPage from "./pages/RegisterPage";
 import ResetPassPage from "./pages/ResetPassPage";
+import UploadResumePage from "./pages/UploadResumePage";
 
 function App() {
   return (
     <div>
-      {/* DESCOMENTE PARA TESTAR PAGINAS KK */}
-      <nav>
-        <ul>
-          <li><Link to="/">Login</Link></li>
-          <li><Link to="/recovery">Recuperar Senha</Link></li>
-          <li><Link to="/register">Registrar</Link></li>
-          <li><Link to="/reset">Reset</Link></li>
-
-        </ul>
-      </nav>
-
       {/* Rotas */}
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/recovery" element={<RecoveryPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/reset" element={<ResetPassPage />} />
+        <Route path="/upload" element={<UploadResumePage />} />
       </Routes>
     </div>
   );
